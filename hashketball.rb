687b7cceb6bb32297f848f1bfe660ec70ef5e06d
Oliver <oliver.grant@gmail.com>
2019-01-27 09:37:00 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -167,24 +167,20 @@ def player_stats(name)
   end
 end
 
-def search_bigmost_stat (search,stat)
-  bigmost = 0
-  answer = 0
+def big_shoe_rebounds
+  biggest_shoe=0
+  rebounds=0
   game_hash.each do |home_away,team_info|
     team_info.each do |team_data,players|
       if team_data == :players 
         players.each do |name,stats|
-          if stats[search] > bigmost
-            bigmost = stats[search]
-            answer = stats[stats]
+          if stats[:shoe] > biggest_shoe
+            biggest_shoe = stats[:shoe]
+            rebounds = stats[:rebounds]
           end
         end
       end
     end
   end
-  answer
-end
-
-def big_shoe_rebounds
-  search_bigmost_stat (:shoe,:rebounds)
+  rebounds
 end
